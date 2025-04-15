4dfcb4d3eaa42a912a219b00803bcccb64f85000
sushil kumar <sushil88081@gmail.com>
2025-04-16 05:12:40 +0530

    struct appointment

--- models/appointment.go
+++ models/appointment.go
@@ -1,11 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Appointment struct {
-	ID              int       `db:"id" json:"id"`
-	AppointmentDate time.Time `db:"Appointment_Date" json:"Appointment_Date"`
-	DoctorID        int       `db:"Doctor_ID" json:"Doctor_ID"`
-	PatientID       int       `db:"Patient_ID" json:"Patient_ID"`
-	Status          string    `db:"Status" json:"Status"`
+	gorm.Model
+	AppointmentId   uint      `gorm:"not null" json:"appointment_id"`
+	PatientID       uint      `gorm:"not null" json:"patient_id"`
+	DoctorID        uint      `gorm:"not null" json:"doctor_id"`
+	AppointmentDate time.Time ` gorm:"TIMPSTAMP"`
+	Status          string    `gorm:"type:varchar(10);not null" json:"status"`
+
+	//belongs to Doctor and patient table
+	Doctor  DoctorList `gorm:"foreignKey:DoctorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"doctor"`
+	Patient Patient    `gorm:"foreignKey:PatientID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"patient"`
 }
